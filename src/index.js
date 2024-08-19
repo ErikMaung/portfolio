d20536e6cf1a807d28494dbfba3e326d03ef4bae
Erik Maung <infiniteerik123@gmail.com>
2024-08-19 02:05:53 -0700

    updated deprecated index render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+// Create a root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render your app using the new root API
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',  // Important for React Router
+    publicPath: '/',  // Important for routing and loading correctly
   },
   module: {
     rules: [
