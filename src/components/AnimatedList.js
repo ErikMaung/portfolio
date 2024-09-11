3bf51011cee2106fe4e7ec6a4d8ace5164dff805
Erik Maung <erikmaung1@gmail.com>
2024-09-11 05:07:46 -0700

    change copy cursor pointer

--- src/components/AnimatedList.js
+++ src/components/AnimatedList.js
@@ -18,6 +18,7 @@ function AnimatedList() {
     setIsVisible(!isVisible);
   };
 
+
   const handleCopy = () => {
     setShowOverlay(true);
     setTimeout(() => {
@@ -69,7 +70,7 @@ function AnimatedList() {
               <CopyToClipboard text={item === email ? 'erikmaung1@gmail.com' : item === phone ? '6266241560' : undefined} onCopy={handleCopy}>
                 <li key={index} className={`list-item ${animate ? 'animate' : ''} ${item === email ? undefined : undefined}`}>
                   <a href={links2[index]} target="_blank" rel="noopener noreferrer">
-                    <span>{item}</span>
+                    <span style={{ cursor: 'pointer' }}>{item}</span>
                     {item === email || item === phone ? <svg className='copy-svg' xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="20" height="20" viewBox="0 0 512 640" fill='#00000080'>
                       <g><path d="M324.896,106.622H107.87c-18.14,0-32.887,14.747-32.887,32.887v320.034c0,18.14,14.747,32.909,32.887,32.909h217.026   c18.14,0,32.909-14.769,32.909-32.909V139.51C357.805,121.369,343.036,106.622,324.896,106.622z M339.621,459.544   c0,8.116-6.608,14.725-14.725,14.725H107.87c-8.116,0-14.725-6.608-14.725-14.725V139.51c0-8.116,6.608-14.725,14.725-14.725   h217.026c8.116,0,14.725,6.608,14.725,14.725V459.544z" /><path d="M406.371,19.548H198.415c-22.431,0-40.671,20.114-40.671,44.829v13.017c0,5.012,4.069,9.081,9.081,9.081   s9.081-4.069,9.081-9.081V64.377c0-14.703,10.101-26.667,22.509-26.667h207.956c6.875,0,12.463,6.863,12.463,15.301v315.832   c0,8.449-5.588,15.302-12.463,15.302h-23.64c-5.012,0-9.092,4.08-9.092,9.092c0,5.012,4.08,9.092,9.092,9.092h23.64   c16.898,0,30.647-15.035,30.647-33.486V53.012C437.018,34.561,423.269,19.548,406.371,19.548z" /></g>
                     </svg> : undefined}
